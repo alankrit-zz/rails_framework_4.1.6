7a590cadcb1f47b76d1a9113ddfea7781bf59a66
alankrit <alankrit@gmail.com>
2014-12-11 04:22:32 +0530

    new changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "users#index"
+  resources :users
+
   namespace :api, defaults: {format: 'json'} do
     resources :doctors, only: [:index, :create, :show]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202051311) do
+ActiveRecord::Schema.define(version: 20141208091838) do
 
   create_table "clinics", force: true do |t|
     t.string   "name"
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 20141202051311) do
     t.string   "day"
     t.integer  "time"
     t.integer  "clinic_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
